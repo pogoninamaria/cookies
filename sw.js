cff2fb12bcb759aa17feaaba7666db10afc7a395
pogoninamaria <pogoninamary@yandex.ru>
2026-07-16 14:25:51 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'fortune-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/predictions.html',
+    '/history.html',
+    '/categories.html',
+    '/feedback.php',
+    '/action.php',
+    '/style.css'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
